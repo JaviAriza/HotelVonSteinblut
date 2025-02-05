ef6f1e5b73c9e60fdbe7cff0ef472ea3593f7aaf
PabloNoriaGomezDEV <kynospablo@hotmail.com>
2025-02-05 10:17:05 +0100

    footer done

---
+++ src/FooterComponent.jsx
@@ -0,0 +1,33 @@
+import { Footer } from "flowbite-react";
+import "./FooterComponent.css";
+import { BsGithub, BsInstagram } from "react-icons/bs";
+import logo from "./assets/logo.png";
+
+
+export default function FooterComponent() {
+  return (
+    <Footer container className="footer-container">
+      <div className="footer-content">
+        <div className="follow">
+          <h4>Síguenos en:</h4>
+          <Footer.Icon href="#" icon={BsInstagram} className="social-icon" />
+          <Footer.Icon href="#" icon={BsGithub} className="social-icon" />
+        </div>
+
+        <div className="derechos">
+          <p>© Hotel Von Steinblut 2025. Todos los derechos reservados.</p>
+          <div className="legal-links">
+            <a href="#">Aviso Legal</a> |
+            <a href="#">Política de Privacidad</a> |
+            <a href="#">Términos y Condiciones</a>
+          </div>
+        </div>
+
+        <div className="logo">
+          <img src={logo} alt="Hotel Von Steinblut Logo" />
+        </div>
+      </div>
+    </Footer>
+  );
+}
+
