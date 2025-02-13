fd871a7925a85769738728ae9213468c828b4240
mortiZimmerman <mauricionicolasortiz@gmail.com>
2025-02-13 11:33:08 +0100

    test: add test for Footer Component

---
+++ src/components/Footer/Footer.test.jsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Footer from "./Footer";
+import { BsGithub, BsInstagram } from "react-icons/bs";
+import { describe, it, expect } from "vitest";
+
+describe("Footer component", () => {
+    it("follow us on:", () => {
+        render(<Footer /> );
+        const followText = screen.getByText(/follow us on:/i);
+        expect(followText).toBeInTheDocument();
+    });
+})
