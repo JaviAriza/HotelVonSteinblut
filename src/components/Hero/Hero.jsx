abee513ed43baf4b17e550e93ff0fcce0b3fcd10
jthamayo <jtamayobalogh@gmail.com>
2025-02-16 19:45:42 +0100

    refactor: remove css files and change css code for tailwind classes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,13 @@
-import "./Hero.css";
-
 const Header = () => {
   return (
-    <div className="background">
+    <div className="w-full h-full">
       <img
-        className="hero-logo"
+        className="fixed w-1/2 sm:w-[250px] left-1/2 top-1/2 -translate-1/2 z-10 transition-all opacity-80"
         src="https://res.cloudinary.com/dw94v5tvs/image/upload/v1739433825/logo_ggptnk.png"
         alt="hotel von Steinblut logo"
       />
       <div
-        className="bg-center bg-cover h-64 w-full h-[1400px]"
+        className="bg-center bg-cover w-full h-[1400px] bg-no-repeat"
         style={{
           backgroundImage:
             "url('https://res.cloudinary.com/dw94v5tvs/image/upload/v1739433825/castle-2_zxiibh.png')",
