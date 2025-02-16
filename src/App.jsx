c7f69d7ea87354a4f426ecc9368a6617dae99b55
Nando218 <fernando.delatorre.design@gmail.com>
2025-02-16 14:09:17 +0100

    feat: add Background component and update styles for Footer and Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import './App.css'
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import Hero from './components/Hero/Hero';
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
+import Background from "./components/Background/Background";
 
 function App() {
-
   return (
     <>
       <Header />
+      <Background colorStops={["#FF0000", "#000000", "#FF0000"]} speed={0.8} />
       <Hero />
-      <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
 
 export default App;
