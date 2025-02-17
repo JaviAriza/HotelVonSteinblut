1f7ed3e00ea2253f29a15a596e9459f734a250b6
JaviAriza <javier.ariza.rosales@alumnos.cei.es>
2025-02-17 09:41:55 +0100

    resolve routes error

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { Router } from './router/Router'
+import { router } from './router/Router'
 import './index.css'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={Router} />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
