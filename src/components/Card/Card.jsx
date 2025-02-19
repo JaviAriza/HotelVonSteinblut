446283fb4e36280a114251d1863905b65ad048df
JaviAriza <javier.ariza.rosales@alumnos.cei.es>
2025-02-19 11:32:16 +0100

    create card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,11 @@
+import "./Card.css"
+export default function Card() {
+  return (
+    <div className="bodyCard">
+        <img className="imgCard" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWi29mOalQDV_Z3OQs5RojJxdAZhjnDjSMYg&s" alt="card image" />
+        <h3>Name</h3>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type</p>
+        <button></button>
+    </div>
+  );
+}
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,13 @@
 import Hero from "../Hero/Hero";
 import Booking from "../Booking/Booking";
+import Card from "../Card/Card"
+
 
 const Main = () => {
     return (
         <main className="flex flex-col items-center justify-between w-full">
             <Hero />
+            <Card />
             <Booking />
         </main>
     );
