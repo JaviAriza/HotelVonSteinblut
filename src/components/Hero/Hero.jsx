126efaa85e3c87451a07c33a382282b1f49a67b9
jthamayo <jtamayobalogh@gmail.com>
2025-02-22 21:32:04 +0100

    fix: remove margin top

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import Logo from '../Logo/Logo'
 
 const Hero = () => {
   return (
-    <div className="w-full h-2/3 mt-[60px] relative">
+    <div className="w-full h-2/3 relative">
       <Logo />
       <div id='hero'
         className="bg-center bg-cover w-full h-[1400px] bg-no-repeat"
