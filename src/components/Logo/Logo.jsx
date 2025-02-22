d426aa91723151880bef74e1ffa5084f5f7ee759
jthamayo <jtamayobalogh@gmail.com>
2025-02-22 21:21:52 +0100

    refactor: remove state from component and update element position with useTrackPosition hook state

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,35 +1,18 @@
-import { useState, useEffect } from "react";
+import useTrackPosition from "../../hooks/useTrackPosition";
+import {
+  getContainerMiddle,
+  getTrackPosition,
+} from "../../utils/positionUtils";
 
 const Logo = () => {
-  const [isFixed, setIsFixed] = useState(true);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const heroHeight = document.getElementById("hero")?.offsetHeight || 0;
-      console.log(heroHeight, window.scrollY);
-
-      if (window.scrollY >= heroHeight * 0.4) {
-        setIsFixed(false);
-      }
-      if (window.scrollY <= heroHeight * 0.4) {
-        setIsFixed(true);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  });
-
+  const isFixed = useTrackPosition("hero");
+  const restingPos = getContainerMiddle("hero") + getTrackPosition("logo");
   return (
     <img
       id="logo"
-      className={` pointer-events-none  transition-all sm:w-[250px] opacity-80 left-1/2 -translate-x-1/2 duration-500 translate-y-1/2 z-10 ${
-        isFixed
-          ? "fixed w-1/2 bottom-1/2"
-          : "absolute bottom-[600px]"
-      }`}
+      className={`${
+        isFixed ? "fixed bottom-1/2" : `absolute top-[${restingPos}px]`
+      }  pointer-events-none sm:w-[250px] w-1/2 opacity-80 left-1/2 -translate-x-1/2 translate-y-1/2 z-10`}
       src="https://res.cloudinary.com/dw94v5tvs/image/upload/v1739433825/logo_ggptnk.png"
       alt="hotel von Steinblut logo"
     />
