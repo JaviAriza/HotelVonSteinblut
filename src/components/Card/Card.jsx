2100189a906f1e7fd2ea3f81c4a17f206730acbc
JaviAriza <javier.ariza.rosales@alumnos.cei.es>
2025-02-23 20:43:31 +0100

    add borderCard

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,9 +3,9 @@ export default function Card() {
   return (
     <div className="bodyCard">
         <img className="imgCard" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWi29mOalQDV_Z3OQs5RojJxdAZhjnDjSMYg&s" alt="card image" />
-        <h3>Name</h3>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type</p>
-        <button></button>
+        <img className="imgBorder" src="https://res.cloudinary.com/dnjosjzrj/image/upload/v1740338442/marcoHab_7_r9qulo.png" alt="card image" />
+        <button className="back" src="https://res.cloudinary.com/dnjosjzrj/image/upload/v1740338995/icon_yfpyxn.png"></button>
+        <button className="next" src="https://res.cloudinary.com/dnjosjzrj/image/upload/v1740338995/icon_yfpyxn.png"></button>
     </div>
   );
 }
