5891a0419b88f03da2d395b4b66727455d72010f
jthamayo <jtamayobalogh@gmail.com>
2025-02-23 19:05:59 +0100

    feat: add component to display services section

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,11 +1,14 @@
 import Hero from "../Hero/Hero";
 import Booking from "../Booking/Booking";
+import RestaurantServices from "../RestaurantServices/RestaurantServices";
 
 const Container = () => {
     return (
         <main className="flex flex-col items-center justify-between w-full h-full">
             <Hero />
             <Booking />
+            <RestaurantServices />
+            <RestaurantServices />
         </main>
     );
 }
---
+++ src/components/RestaurantServices/RestaurantServices.jsx
@@ -0,0 +1,27 @@
+import ExpandablePanel from "../ExpandablePanel/ExpandablePanel";
+
+const RestaurantServices = () => {
+  return (
+    <>
+      <section className="my-[20%] restaurant-services w-full h-screen bg-black flex flex-col sm:flex-row">
+        <ExpandablePanel
+          image={
+            "https://res.cloudinary.com/dw94v5tvs/image/upload/v1739958637/image_13_oz5cyv.png"
+          }
+          isLeadPanel={true}
+          text={"cena grupal"}
+        />
+        <span className="h-[1px] w-full sm:h-full sm:w-[1px] border-b sm:border-l border-white"></span>
+        <ExpandablePanel
+          image={
+            "https://res.cloudinary.com/dw94v5tvs/image/upload/v1739958637/image_13_oz5cyv.png"
+          }
+          isLeadPanel={false}
+          text={"cena con espectáculo"}
+        />
+      </section>
+    </>
+  );
+};
+
+export default RestaurantServices;
