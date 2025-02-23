b9fc3e9f7d1f1e2cf5fa4ec71c940c7502b3716e
Nando218 <fernando.delatorre.design@gmail.com>
2025-02-23 20:16:40 +0100

    feat: update Title component with new ornament images and adjust layout (not finished yet)

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,14 +1,15 @@
 export default function Title() {
-    return (
-      <div className="bg-transparent text-white text-center py-6 absolute left-1/2 bottom-1/4 -translate-x-1/2 w-3/4">
-        <div className="border-t border-gray-500 w-3/4 mx-auto mb-2"></div>
-        <h1 className="text-2xl font-serif tracking-wide">
-          PROBANDO TÍTULOS
-        </h1>
-        <div className="mt-2">
-          <img src="https://res.cloudinary.com/dgzgzx9ov/image/upload/v1740045848/Group_62_minvw4.png" alt="Filigrana" className="mx-auto w-48" />
-        </div>
+  return (
+    <div className="bg-transparent text-white text-center py-6 absolute left-1/2 bottom-1/4 -translate-x-1/2 w-3/4">
+      <div className="mt-2">
+        <img src="https://res.cloudinary.com/dgzgzx9ov/image/upload/v1740335845/Ornament2_rwlwvz.png" alt="Filigrana superior" className="mx-auto w-200 h-5" />
       </div>
-    );
+      <h1 className="text-2xl font-serif tracking-wide">
+        PROBANDO TÍTULOS
+      </h1>
+      <div className="mt-2">
+        <img src="https://res.cloudinary.com/dgzgzx9ov/image/upload/v1740335763/Ornament1_ogcwyx.png" alt="Filigrana" className="mx-auto w-90" />
+      </div>
+    </div>
+  );
 }
-  
