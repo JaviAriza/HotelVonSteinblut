b4d6a9150cfbf9c977170c7137530e2ba8959134
JaviAriza <javier.ariza.rosales@alumnos.cei.es>
2025-02-24 12:07:02 +0100

    create formLogin

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,15 +2,29 @@ import "./Login.css";
 
 const login = () => {
     return (
-       <form action="" className="formLogin">
-            <label htmlFor="Gmail">Gmail</label>
+      <div>
+         <form action="" className="loginForm">
+            <label htmlFor="Email">Email</label>
             <input type="text" className="inputGmail" required/>   
             <label htmlFor="Contraseña">Contraseña</label>
             <input type="text" className="inputContraseña" required/>
             <button type="submit" className="btnLogin">Iniciar Sesión</button>
        </form>   
+       <p id="error"></p>
+      </div>
+       
     );
   };
   
+   document.getElementById("loginForm").addEventListener("submit", function(event){
+       event.preventDefault();
+       const email = document.getElementById("inputGmail").value;
+       const contraseña = document.getElementById("inputContraseña").value;
+       const error = document.getElementById("error");
+
+       if(!email.includes("@")){
+           error.textContent = "Correo inválido";
+   });
+
   export default login;
   
