e785fee8f9f4dd861bd4bf142dfaa9ad72c54c68
JaviAriza <javier.ariza.rosales@alumnos.cei.es>
2025-02-24 19:01:53 +0100

    feat: complete login validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Login from "./components/login/Login";
+import Login from "./components/Login/Login";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <>
       <Header />
       <Login />
-     
+      
+      
     </>
   );
 }
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,30 +1,74 @@
+import React from "react";
+import { useForm } from "react-hook-form";
 import "./Login.css";
 
-const login = () => {
-    return (
-      <div>
-         <form action="" className="loginForm">
-            <label htmlFor="Email">Email</label>
-            <input type="text" className="inputGmail" required/>   
-            <label htmlFor="Contraseña">Contraseña</label>
-            <input type="text" className="inputContraseña" required/>
-            <button type="submit" className="btnLogin">Iniciar Sesión</button>
-       </form>   
-       <p id="error"></p>
-      </div>
-       
-    );
+function login() {
+  const {
+    register,
+    handleSubmit, 
+    formState: { errors }, 
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
   };
-  
-   document.getElementById("loginForm").addEventListener("submit", function(event){
-       event.preventDefault();
-       const email = document.getElementById("inputGmail").value;
-       const contraseña = document.getElementById("inputContraseña").value;
-       const error = document.getElementById("error");
 
-       if(!email.includes("@")){
-           error.textContent = "Correo inválido";
-   });
+  return (
+    <form className="formLogin" onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="email">Email   </label>
+        <input
+          id="email"
+          type="email"
+          placeholder="ejemplo@correo.com"
+          autoComplete="off"
+          {...register("email", {
+            required: "El correo es obligatorio",
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: "Ingrese un correo válido"
+            },
+            validate: {
+              noSpaces: (value) =>
+                !/\s/.test(value) || "El correo no debe contener espacios",
+              validDomain: (value) =>
+                /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com|net|org|edu|gov|info|io|co|es|mx)$/i.test(value) ||
+                "El dominio no es válido",
+            }
+          })}
+        />
+        {errors.email && <p>{errors.email.message}</p>}
+      </div>
 
-  export default login;
-  
+      <div>
+        <label htmlFor="contraseña">contraseña   </label>
+        <input
+          id="password"
+          type="password"
+          placeholder="Ingresa tu contraseña"
+          autoComplete="off"
+          {...register("password", {
+            required: "La contraseña es obligatoria",
+            minLength: {
+              value: 8,
+              message: "Debe tener al menos 8 caracteres"
+            },
+            maxLength: {
+              value: 20,
+              message: "No debe superar los 20 caracteres"
+            },
+            pattern: {
+              value: /^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/,
+              message: "Debe incluir una mayúscula, un número y un símbolo"
+            }
+          })}
+        />
+        {errors.password && <p>{errors.password.message}</p>}
+      </div>
+
+      <button type="submit">Enviar</button>
+    </form>
+  );
+}
+
+export default login;
