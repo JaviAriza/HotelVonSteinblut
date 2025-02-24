82ebc9fad0fb995c6f36023ea8b42cbe533351e2
jthamayo <jtamayobalogh@gmail.com>
2025-02-24 10:34:48 +0100

    refactor: add props validation

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -2,11 +2,11 @@ import PropTypes from 'prop-types'
 
 export default function Title(props) {
   return (
-    <div className="bg-transparent text-white text-center py-6 absolute left-1/2 -translate-x-1/2 w-3/4 my-20">
+    <div className="bg-transparent text-white text-center py-6 absolute left-1/2 -translate-x-1/2 w-3/4 my-10">
       <div className="mt-2">
-        <img src="https://res.cloudinary.com/dgzgzx9ov/image/upload/v1740335845/Ornament2_rwlwvz.png" alt="Filigrana superior" className="mx-auto w-200 h-5" />
+        <img src="https://res.cloudinary.com/dgzgzx9ov/image/upload/v1740335845/Ornament2_rwlwvz.png" alt="Filigrana superior" className="mx-auto w-full h-5" />
       </div>
-      <h1 className="text-2xl font-serif tracking-widest uppercase">
+      <h1 className="text-5xl font-serif tracking-widest uppercase">
         {props.text}
       </h1>
       <div className="mt-2">
