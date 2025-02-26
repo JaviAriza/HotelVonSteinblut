34b5972fc4ac41c712bff6f75cf1f27ddbf550a5
jthamayo <jtamayobalogh@gmail.com>
2025-02-26 09:51:55 +0100

    fix: remove merge remaining indicators

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,18 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Background from "./components/Background/Background";
-<<<<<<< HEAD
-import { Outlet } from "react-router-dom";
-import EmailJS from "./components/EmailJS/Email";
-
-function App() {
-  return (
-    
-      
-      <EmailJS />
-
-=======
 import { Outlet } from 'react-router-dom';
-import Container from "./components/Container/Container";
-import Fetch from "./components/Fetch/Fetch";
 
 function App() {
   return (
     <>
       <Header />
       <Background colorStops={["#FF0000", "#000000", "#FF0000"]} speed={0.8} />
+      <main className="flex flex-col items-center justify-between w-full">
       <Outlet />
-      <Container />
-      <Fetch />
+      </main>
       <Footer />
     </>
->>>>>>> origin/data
   );
 }
 
