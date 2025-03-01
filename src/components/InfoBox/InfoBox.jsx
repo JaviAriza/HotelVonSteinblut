5ce9386054d1d520d148701ae63cf2fa1894f172
mortiZimmerman <mauricionicolasortiz@gmail.com>
2025-03-01 19:36:30 +0100

    feat:css to tailwind infoBox

--- src/components/InfoBox/InfoBox.jsx
+++ src/components/InfoBox/InfoBox.jsx
@@ -6,42 +6,61 @@ function InfoBox({ room, onClose }) {
   if (!room) return null;
 
   return (
-    <div className="info-overlay" onClick={onClose}>
-      <div className="info-content" onClick={(e) => e.stopPropagation()}>
+    <div
+      className="info-overlay fixed top-0 left-0 w-full h-full bg-black/50 flex justify-center items-center z-[1000] text-lg"
+      onClick={onClose}
+    >
+      <div
+        className="info-content bg-[#5f5c5c] flex p-5 w-1/2 h-[60%] rounded-lg gap-4"
+        onClick={(e) => e.stopPropagation()}
+      >
         <img
-          className="infoBox-img"
+          className="infoBox-img w-[40%] rounded-xl object-cover"
           src={room.photos[0]}
           alt="Imagen de la habitación"
         />
 
-        <h2>Room {room.type}</h2>
-        <p>
-          <strong>ability:</strong> {room.capacity} personas
-        </p>
-        <p>
-          <strong>availability:</strong> {room.availability}
-        </p>
-        <p>
-          <strong>Price per night:</strong> {room.nightPrice} €
-        </p>
+        <div className="content">
+          <div className="roomItems whitespace-nowrap">
+            <h2 className="text-4xl text-center">Room {room.type}</h2>
+            <p>
+              <strong>Ability:</strong> {room.capacity} personas
+            </p>
+            <p>
+              <strong>Availability:</strong> {room.availability}
+            </p>
+            <p>
+              <strong>Price per night:</strong> {room.nightPrice} €
+            </p>
+          </div>
 
-        <ul className="ul-feature-list">
-          <li>
-            <strong>Beds:</strong> {room.beds}
-          </li>
-          <li>
-            <strong>air conditioning:</strong> {room.features.airConditioning}
-          </li>
-          <li>
-            <strong>WiFi:</strong> {room.features.wifi}
-          </li>
-          <li>
-            <strong>TV:</strong> {room.features.tv}
-          </li>
-        </ul>
+          <img
+            src="https://res.cloudinary.com/dgzgzx9ov/image/upload/v1740397694/OrnamentLine1_xrziiw.png"
+            alt="spacebar"
+            className="image-space mt-5"
+          />
 
-        <class className="buttons">
-          <button className="close-btn bg-red p-2 px-4 rounded-md cursor-pointer mt-[330px] ml-[-390px]" onClick={onClose}>
+          <div className="ul-feature-list-container mt-5">
+            <h1 className="text-2xl">Features:</h1>
+            <ul className="ul-content-list grid grid-cols-2 gap-x-10 whitespace-nowrap">
+              <li>
+                <strong>Beds:</strong> {room.beds}
+              </li>
+              <li>
+                <strong>Air conditioning:</strong> {room.features.airConditioning}
+              </li>
+              <li>
+                <strong>WiFi:</strong> {room.features.wifi}
+              </li>
+              <li>
+                <strong>TV:</strong> {room.features.tv}
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="buttons">
+          <button className="close-btn bg-red p-2 px-4 rounded-md cursor-pointer mt-[330px] ml-[-330px]" onClick={onClose}>
             Close
           </button>
           <button className="book-btn bg-red p-2 px-4 rounded-md cursor-pointer mt-[90px] ml-[90px]"
@@ -49,7 +68,7 @@ function InfoBox({ room, onClose }) {
           >
             Book
           </button>
-          </class>
+        </div>
       </div>
     </div>
   );
