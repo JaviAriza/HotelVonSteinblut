dfd4c7f3ce7aabb50a372cb1d71066333a38c646
jthamayo <jtamayobalogh@gmail.com>
2025-03-01 13:16:45 +0100

    fix: translate room object access properties to english in InfoBox component

--- src/components/InfoBox/InfoBox.jsx
+++ src/components/InfoBox/InfoBox.jsx
@@ -1,27 +1,27 @@
 import "./InfoBox.css";
 
 function InfoBox({ room, onClose }) {
-  if (!room) return null; // Evita errores si no se pasa la room
+  if (!room) return null;
 
   return (
     <div className="info-overlay" onClick={onClose}>
       <div className="info-content" onClick={(e) => e.stopPropagation()}>
         
         
-        <img className="infoBox-img" src={room.fotos[0]} alt="Imagen de la habitación" />
+        <img className="infoBox-img" src={room.photos[0]} alt="Imagen de la habitación" />
 
-        <h2>Room {room.tipo}</h2>
-        <p><strong>ability:</strong> {room.capacidad} personas</p>
-        <p><strong>availability:</strong> {room.disponibilidad}</p>
-        <p><strong>Price per night:</strong> {room.precioNoche} €</p>
+        <h2>Room {room.type}</h2>
+        <p><strong>ability:</strong> {room.capacity} personas</p>
+        <p><strong>availability:</strong> {room.availability}</p>
+        <p><strong>Price per night:</strong> {room.nightPrice} €</p>
 
     
         <ul className="ul-feature-list">
           <button className="close-btn" onClick={onClose}>Close</button>
-          <li><strong>Beds:</strong> {room.camas}</li>
-          <li><strong>air conditioning:</strong> {room.caracteristicas.climatizacion}</li>
-          <li><strong>WiFi:</strong> {room.caracteristicas.wifi}</li>
-          <li><strong>TV:</strong> {room.caracteristicas.tv}</li>
+          <li><strong>Beds:</strong> {room.beds}</li>
+          <li><strong>air conditioning:</strong> {room.features.airConditioning}</li>
+          <li><strong>WiFi:</strong> {room.features.wifi}</li>
+          <li><strong>TV:</strong> {room.features.tv}</li>
         </ul>
 
       </div>
