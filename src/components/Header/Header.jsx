93685b2aff51c18de46c67cd8a87d9f8103d59f0
Nando218 <fernando.delatorre.design@gmail.com>
2025-03-01 09:49:27 +0100

    style: update Header component with backdrop blur and logo adjustments

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
 
   return (
       <header className='fixed top-0 w-full z-100 h-[60px]'>
-        <nav className="navbar [background-color:transparent] h-full flex items-center justify-between text-white">
+        <nav className="navbar backdrop-blur-[3px] h-full flex items-center justify-between text-white">
           <div
             className={`nav_toggle ${isOpen ? "open" : ""}`}
             onClick={handleToggle}
@@ -20,15 +20,17 @@ function Header() {
             <span></span>
             <span></span>
           </div>
-          <div className="mx-3 nav_logo size-10"><img src="https://res.cloudinary.com/dw94v5tvs/image/upload/v1739433825/logo_ggptnk.png" alt="hotel-logo" /></div>
+          <div className="mx-3 nav_logo size-20 mt-11" onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}><img src="https://res.cloudinary.com/dgzgzx9ov/image/upload/v1739791107/VS_chiquito_hshofp.png" alt="hotel-logo" /></div>
           <ul className={`backdrop-blur-[5px] nav_items flex ${isOpen ? "open" : ""}`}>
-            <li ><Link to="/">Hotel</Link></li>
-            <li ><Link to="/restaurant">Restaurant</Link></li>
-            <li ><Link to="/reservations">Rooms</Link></li>
-            <li ><Link to="/restaurant">Services</Link></li>
-            <li ><Link to="/restaurant">Gallery</Link></li>
-            <li ><Link to="/restaurant">FAQ</Link></li>
-            <li ><Link to="/customer">Profile</Link></li>
+                      <li><Link to="/">Hotel</Link></li>
+          <li><Link to="/restaurant">Restaurant</Link></li>
+          <li><Link to="/reservations">Rooms</Link></li>
+          <li><Link to="/restaurant">Services</Link></li>
+          <li><Link to="/restaurant">Gallery</Link></li>
+          <li><Link to="/restaurant">FAQ</Link></li>
+          <li><Link to="/customer">Profile</Link></li>
+          <li><Link to="/login">Login</Link></li>
+
           </ul>
         </nav>
       </header>
