8384edc27b69fba0b2512f3f8da4c73ab5cf947c
Nando218 <fernando.delatorre.design@gmail.com>
2025-03-02 09:11:57 +0100

    refactor: update Hero component background styling with gradient overlay

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,13 @@
-import Logo from '../Logo/Logo'
+import Logo from "../Logo/Logo";
 const Hero = (props) => {
   return (
     <div className="w-full h-[1400px] relative -z-10">
       <Logo />
-      <div id="hero"
+      <div
+        id="hero"
         className="bg-center bg-cover w-full h-[1400px] bg-no-repeat"
         style={{
-          backgroundImage: `url(${props.image})`,
+          backgroundImage: `linear-gradient(to bottom, transparent, #1a1a1a 99.9%), url(${props.image})`,
         }}
       ></div>
     </div>
