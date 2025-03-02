fdb3586d762f7b1f647f586e104d903723571d87
mortiZimmerman <mauricionicolasortiz@gmail.com>
2025-03-02 13:35:52 +0100

    feat:Added Tailwind to FAQ

--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -51,23 +51,23 @@ const FAQ = () => {
   };
 
   return (
-    <div className="max-w-4xl mx-auto px-4 py-8">
-      <h2 className="text-3xl font-semibold text-center mb-6">Frequently Asked Questions - Hotel Von Steinblut</h2>
+    <div className="max-w-6xl mx-auto px-4 py-8">
+      <h2 className="text-3xl font-semibold text-center mb-6 mt-15 text-white mb-15">Frequently Asked Questions - Hotel Von Steinblut</h2>
       <div className="space-y-4">
         {faqs.map((faq, index) => (
           <div
             key={index}
-            className="bg-black p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300"
+            className="bg-black p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 -mt-6"
           >
             <div
               className="flex justify-between items-center cursor-pointer text-lg font-semibold text-gray-800"
               onClick={() => toggleAnswer(index)}
             >
-              <h3>{faq.question}</h3>
-              <span className="text-xl">{activeIndex === index ? '-' : '+'}</span>
+              <div className='text-2xl text-white'>{faq.question}</div>
+              <span className="text-xl text-red-800">{activeIndex === index ? '-' : '+'}</span>
             </div>
             {activeIndex === index && (
-              <div className="mt-4 text-gray-600 text-sm">
+              <div className="mt-4 text-white text-md">
                 <p>{faq.answer}</p>
               </div>
             )}
