c2aa3873b03042181df55887572edfced623c6e6
mortiZimmerman <mauricionicolasortiz@gmail.com>
2025-03-03 08:56:34 +0100

    "merge: merge dev to profileView"

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,9 +4,15 @@ const Profile = () => {
         <div className="bg-black text-white w-[80vh] min-h-[140vh] rounded-lg shadow-lg p-6">
          <div>
             <h1 className="text-2xl mt-20 ml-10">PROFILE</h1>
-            ñ
+            
          </div>
+         
+         <img src="https://res.cloudinary.com/dgzgzx9ov/image/upload/v1740385677/ornamentCorner1_xe1anj.png" alt="left_top_corner" />
+         <img src="https://res.cloudinary.com/dgzgzx9ov/image/upload/v1740385677/ornamentCorner1_xe1anj.png" alt="rigth_top_corner"/>
+         <img src="https://res.cloudinary.com/dgzgzx9ov/image/upload/v1740385677/ornamentCorner1_xe1anj.png" alt="left_bottom_corner"/>
+         <img src="https://res.cloudinary.com/dgzgzx9ov/image/upload/v1740385677/ornamentCorner1_xe1anj.png" alt="rigth_bottom_corner"/>
         </div>
+        
       </div>
     );
   };
